617af5c7ae660f077149dea03926f218c1fee065
Ryan Leontini <79189079+ryanleontini@users.noreply.github.com>
2024-05-13 20:42:52 -0700

    Receive table working

--- A7-Wireless.c
+++ A7-Wireless.c
@@ -18,42 +18,163 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
 #include "spi.h"
 #include "usart.h"
 #include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include "spsgrf.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
 #include "SPIRIT_Config.h"
 #include "networkTable.h"
 
-#include "spsgrf.h"
-#include <stdio.h>
+/* USER CODE END Includes */
 
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+extern UART_HandleTypeDef huart2;
+#define STACK_SIZE 512
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+volatile SpiritFlagStatus xTxDoneFlag;
+volatile SpiritFlagStatus xRxDoneFlag;
+TaskHandle_t xReceiveTaskHandle = NULL;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+void MX_FREERTOS_Init(void);
+/* USER CODE BEGIN PFP */
+void Spirit_Register_Init(void);
+void updateNodesTable(void);
+void transmit(void);
+void receive(void);
+void clearScreen(void);
+void heartBeatTask(void);
 
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
+  /* USER CODE BEGIN 1 */
 
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
   SystemClock_Config();
 
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_SPI1_Init();
 
+  /* USER CODE BEGIN 2 */
   SPSGRF_Init();
-  SpiritPktStackSetDestinationAddress(R_LEONTINI); // Me
+//  Spirit_Register_Init();
+  SpiritPktStackSetDestinationAddress(0xFF); // Me
 
+  initNodesArray();
+//  addNode(0x45, "John1", 5);
+//  addNode(0x41, "John2", 5);
+//  addNode(0x42, "John3", 5);
+//  addNode(0x43, "John4", 5);
+//  addNode(0x44, "John5", 5);
+//  addNode(0x46, "John6", 23);
+//  addNode(0x47, "John7", 23);
+//  addNode(0x48, "John8", 23);
+//  addNode(0x49, "John9", 23);
+//  addNode(0x50, "John10", 23);
+  clearScreen();
+
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+//  osKernelInitialize();
+//
+//  /* Call init function for freertos objects (in freertos.c) */
+//  MX_FREERTOS_Init();
+//
+//  /* State Machine Task */
+//
+//  /* Data processing task */
+//
+//  /* Display UART Task */
+//  if (xTaskCreate(updateNodesTable, "HelloWorld", STACK_SIZE, NULL, tskIDLE_PRIORITY + 4, NULL) != pdPASS){ while(1); }
+//  if (xTaskCreate(receive, "receive", STACK_SIZE, NULL, tskIDLE_PRIORITY + 0, &xReceiveTaskHandle) != pdPASS){ while(1); }
+//  if (xTaskCreate(heartBeatTask, "heartbeat", STACK_SIZE, NULL, tskIDLE_PRIORITY + 0, NULL) != pdPASS){ while(1); }
+//
+//  /* Heartbeat task */
+//
+//  /* Start scheduler */
+//  osKernelStart();
+
+
+//  vTaskStartScheduler();
+
+  /* We should never get here as control is now taken by the scheduler */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
   while (1)
   {
     /* USER CODE END WHILE */
-	    HAL_UART_Transmit(&huart2, "Payload Sent\r\n", 14, HAL_MAX_DELAY);
-	    HAL_Delay(500); // Block for 2000 ms
+	  transmit();
+
+//	  displayNodesTable();
+//	  HAL_Delay(1000);
+//	  transmit();
 
     /* USER CODE BEGIN 3 */
+
   }
   /* USER CODE END 3 */
 }
 
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -101,7 +222,158 @@ void SystemClock_Config(void)
 
 /* USER CODE BEGIN 4 */
 
+void heartBeatTask(void) {
+    const TickType_t xDelay = 4000 / portTICK_PERIOD_MS; // 4s delay
+
+	vTaskDelay( xDelay );
+
+    if (xReceiveTaskHandle != NULL) {
+        vTaskSuspend(xReceiveTaskHandle);
+    }
+
+    SpiritCmdStrobeSabort();
+
+    transmit();
+
+    if (xReceiveTaskHandle != NULL) {
+        vTaskResume(xReceiveTaskHandle);
+    }
+
+	// Abort receive for TX
+//    SpiritCmdStrobeSabort();
+
+
+}
+
+void updateNodesTable(void) {
+	while(1) {
+		displayNodesTable();
+		decrementNodesTable();
+	}
+}
+
+void clearScreen(void) {
+	  uint16_t length;
+
+	char *clear = "\x1B[2J\x1B[H";
+	length = strlen(clear);
+	HAL_UART_Transmit(&huart2, (uint8_t *)clear, length, 100);
+	char *cursor = "\x1B[H";
+	length = strlen(cursor);
+	HAL_UART_Transmit(&huart2, (uint8_t *)cursor, length, 100);
+	char *ready = "Ready to receive. \n";
+	length = strlen(ready);
+	HAL_UART_Transmit(&huart2, (uint8_t *)ready, length, 100);
+}
+
+void receive(void) {
+    /* Receive */
+	while(1) {
+
+		char payload[20] = "Hello World!\r\n";
+
+		uint8_t rxLen;
+
+		xRxDoneFlag = S_RESET;
+		SPSGRF_StartRx();
+		while (!xRxDoneFlag);
+	//	xRxDoneFlag = S_RESET;
+
+	    uint8_t sourceAddress;
+	    sourceAddress = SpiritPktStackGetReceivedSourceAddress();
+
+
+	    char* name = getNameFromHex(sourceAddress);
+
+	    addNode(sourceAddress, name, 110);
+
+	}
+
+}
+//
+void transmit(void) {
+	/* Transmit */
+	while(1) {
+		char payload[20] = "Hello World!\r\n";
+		xTxDoneFlag = S_RESET;
+		SPSGRF_StartTx(payload, strlen(payload));
+		while(!xTxDoneFlag);
+	//	xTxDoneFlag = S_RESET;
+
+		HAL_UART_Transmit(&huart2, "Payload Sent\r\n", 14, HAL_MAX_DELAY);
+	}
+
+}
+//
+//void Spirit_Register_Init(void) {
+////	SpiritSpiWriteRegisters(cRegAddress, cNbBytes, pcBuffer);
+//
+//}
+
+//void displayNodesTable(void) {
+//    char *clearScreen = "\x1B[H\x1B[2K"; // Command to clear the screen
+//    char *moveCursor = "\x1B[H"; // Command to move cursor to the top left
+//
+//    uint16_t clearScreenLength = strlen(clearScreen);
+//    uint16_t moveCursorLength = strlen(moveCursor);
+//
+//    HAL_UART_Transmit(&huart2, (uint8_t *)clearScreen, clearScreenLength, 100); // Transmit with data length and timeout
+//    HAL_UART_Transmit(&huart2, (uint8_t *)moveCursor, moveCursorLength, 100); // Transmit with data length and timeout
+//
+//}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  SpiritIrqs xIrqStatus;
+
+  if (GPIO_Pin != SPIRIT1_GPIO3_Pin)
+  {
+    return;
+  }
+
+  SpiritIrqGetStatus(&xIrqStatus);
+  if (xIrqStatus.IRQ_TX_DATA_SENT)
+  {
+    xTxDoneFlag = S_SET;
+    SpiritIrqClearStatus();
+
+  }
+  if (xIrqStatus.IRQ_RX_DATA_READY)
+  {
+    xRxDoneFlag = S_SET;
+    SpiritIrqClearStatus();
+
+  }
+  if (xIrqStatus.IRQ_RX_DATA_DISC || xIrqStatus.IRQ_RX_TIMEOUT)
+  {
+    SpiritCmdStrobeRx();
+    SpiritIrqClearStatus();
+
+  }
+}
+
 /* USER CODE END 4 */
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM16 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM16) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
